61d7ecb90e1fef97383a3f9324dbf973ca14098e
ThomasB123 <44202676+ThomasB123@users.noreply.github.com>
2020-09-16 15:31:10 +0100

    update game

--- game.py
+++ game.py
@@ -3,12 +3,74 @@ import random
 import itertools
 
 def setup():
-    roundLabel = makeLabel('Round {}'.format(str(r)),30,900,0)
-    showLabel(roundLabel)
+    # 5 10 20 25 50 100
+    # red pink blue purple orange brown
+
+    Pred = makeSprite('chips/redChip.png') # 5
+    Ppink = makeSprite('chips/pinkChip.png') # 10
+    Pblue = makeSprite('chips/LblueChip.png') # 20
+    Ppurple = makeSprite('chips/purpleChip.png') # 25
+    Porange = makeSprite('chips/orangeChip.png') # 50
+    Pbrown = makeSprite('chips/brownChip.png') # 100
+    transformSprite(Pred,0,0.08)
+    transformSprite(Ppink,0,0.08)
+    transformSprite(Pblue,0,0.08)
+    transformSprite(Ppurple,0,0.08)
+    transformSprite(Porange,0,0.08)
+    transformSprite(Pbrown,0,0.08)
+    moveSprite(Pred,700,500)
+    moveSprite(Ppink,750,500)
+    moveSprite(Pblue,800,500)
+    moveSprite(Ppurple,850,500)
+    moveSprite(Porange,900,500)
+    moveSprite(Pbrown,950,500)
+    showSprite(Pred)
+    showSprite(Ppink)
+    showSprite(Pblue)
+    showSprite(Ppurple)
+    showSprite(Porange)
+    showSprite(Pbrown)
+    showLabel(makeLabel('5',20,717,550))
+    showLabel(makeLabel('10',20,761,550))
+    showLabel(makeLabel('20',20,812,550))
+    showLabel(makeLabel('25',20,862,550))
+    showLabel(makeLabel('50',20,912,550))
+    showLabel(makeLabel('100',20,955,550))
+
+    Ored = makeSprite('chips/redChip.png') # 5
+    Opink = makeSprite('chips/pinkChip.png') # 10
+    Oblue = makeSprite('chips/LblueChip.png') # 20
+    Opurple = makeSprite('chips/purpleChip.png') # 25
+    Oorange = makeSprite('chips/orangeChip.png') # 50
+    Obrown = makeSprite('chips/brownChip.png') # 100
+    transformSprite(Ored,0,0.08)
+    transformSprite(Opink,0,0.08)
+    transformSprite(Oblue,0,0.08)
+    transformSprite(Opurple,0,0.08)
+    transformSprite(Oorange,0,0.08)
+    transformSprite(Obrown,0,0.08)
+    moveSprite(Ored,700,25)
+    moveSprite(Opink,750,25)
+    moveSprite(Oblue,800,25)
+    moveSprite(Opurple,850,25)
+    moveSprite(Oorange,900,25)
+    moveSprite(Obrown,950,25)
+    showSprite(Ored)
+    showSprite(Opink)
+    showSprite(Oblue)
+    showSprite(Opurple)
+    showSprite(Oorange)
+    showSprite(Obrown)
+    showLabel(makeLabel('5',20,717,0))
+    showLabel(makeLabel('10',20,761,0))
+    showLabel(makeLabel('20',20,812,0))
+    showLabel(makeLabel('25',20,862,0))
+    showLabel(makeLabel('50',20,912,0))
+    showLabel(makeLabel('100',20,955,0))
 
     potLabel = makeLabel(str(pot),50,850,250)
-    playerLabel = makeLabel(str(playerChips),40,650,500)
-    opponentLabel = makeLabel(str(opponentChips),40,650,50)
+    playerLabel = makeLabel(str(playerChips),40,575,525)
+    opponentLabel = makeLabel(str(opponentChips),40,575,10)
     showLabel(potLabel)
     showLabel(playerLabel)
     showLabel(opponentLabel)
@@ -17,8 +79,8 @@ def setup():
     opponentB = makeSprite('redCircle.png')
     transformSprite(playerB,0,0.02)
     transformSprite(opponentB,0,0.02)
-    moveSprite(playerB,460,525)
-    moveSprite(opponentB,460,25)
+    moveSprite(playerB,465,525)
+    moveSprite(opponentB,465,10)
     showSprite(playerB)
     showSprite(opponentB)
 
@@ -47,8 +109,11 @@ def setup():
     showSprite(oppFold)
     showSprite(oppCall)
     showSprite(oppRaise)
-
-    return roundLabel,potLabel,playerLabel,opponentLabel,playerB,opponentB,plaFold,plaCall,plaRaise,oppFold,oppCall,oppRaise
+    # 5 10 20 25 50 100
+    # red pink blue purple orange brown
+    Pchips = [Pred,Ppink,Pblue,Ppurple,Porange,Pbrown]
+    Ochips = [Ored,Opink,Oblue,Opurple,Oorange,Obrown]
+    return Pchips,Ochips,potLabel,playerLabel,opponentLabel,playerB,opponentB,plaFold,plaCall,plaRaise,oppFold,oppCall,oppRaise
 
 #suitDict = {'C':'Clubs','D':'Diamonds','H':'Hearts','S':'Spades'}
 #rankDict = {'2':'Two','3':'Three','4':'Four','5':'Five','6':'Six','7':'Seven','8':'Eight','9':'Nine','10':'Ten','J':'Jack','Q':'Queen','K':'King','A':'Ace'}
@@ -146,6 +211,7 @@ def showAll(x): # x is who folded, 0 for no-one (game reached end), 1 for player
     global pot
     global playerChips
     global opponentChips
+    global r
     pHigh = 0
     for i in list(itertools.combinations(flop + player,5)):
         pHigh = max(pHigh,getHand(i))
@@ -185,6 +251,7 @@ def showAll(x): # x is who folded, 0 for no-one (game reached end), 1 for player
         winner = makeLabel('Player Wins!',120,150,200)
     showLabel(winner)
     game = False
+    r = 1
     return winner
 
 def deal():
@@ -208,11 +275,11 @@ def deal():
     transformSprite(f3,0,0.1)
     transformSprite(f4,0,0.1)
     transformSprite(f5,0,0.1)
-    moveSprite(f1,250,250)
-    moveSprite(f2,350,250)
-    moveSprite(f3,450,250)
-    moveSprite(f4,550,250)
-    moveSprite(f5,650,250)
+    moveSprite(f1,250,230)
+    moveSprite(f2,350,230)
+    moveSprite(f3,450,230)
+    moveSprite(f4,550,230)
+    moveSprite(f5,650,230)
     showSprite(f1)
     showSprite(f2)
     showSprite(f3)
@@ -234,8 +301,8 @@ def deal():
     addSpriteImage(o2,'cards/' + deck[8][0]+deck[8][1] + '.png')
     transformSprite(o1,0,0.1)
     transformSprite(o2,0,0.1)
-    moveSprite(o1,400,100)
-    moveSprite(o2,500,100)
+    moveSprite(o1,400,75)
+    moveSprite(o2,500,75)
     showSprite(o1)
     showSprite(o2)
     return flop,player,opponent,f1,f2,f3,f4,f5,p1,p2,o1,o2
@@ -248,7 +315,8 @@ def play():
     global opponentChips
     # Fold, Call, Raise buttons
     # Once all called, flop, then after all called reveal 4th, then 5th, then all call then show
-    #game = True
+    game = True
+    r = 1
     while game:
         if spriteClicked(plaCall) or spriteClicked(oppCall):
             if r == 1:
@@ -261,7 +329,6 @@ def play():
                 nextSpriteImage(f5)
                 winner = showAll(0)
             r += 1
-            changeLabel(roundLabel,'Round {}'.format(r))
             allCall = False
         elif spriteClicked(playerB): # toggle player cards
             nextSpriteImage(p1)
@@ -269,20 +336,52 @@ def play():
         elif spriteClicked(opponentB): # toggle opponent cards
             nextSpriteImage(o1)
             nextSpriteImage(o2)
-        elif spriteClicked(plaRaise): # player raises
+        # 5 10 20 25 50 100
+        # red pink blue purple orange brown
+        elif spriteClicked(Pred) and playerChips >= 5:
+            pot += 5
+            playerChips -= 5
+        elif spriteClicked(Ppink) and playerChips >= 10:
+            pot += 10
+            playerChips -= 10
+        elif spriteClicked(Pblue) and playerChips >= 20:
+            pot += 20
+            playerChips -= 20
+        elif spriteClicked(Ppurple) and playerChips >= 25:
+            pot += 25
+            playerChips -= 25
+        elif spriteClicked(Porange) and playerChips >= 50:
             pot += 50
             playerChips -= 50
-            changeLabel(potLabel,str(pot))
-            changeLabel(playerLabel,str(playerChips))
-        elif spriteClicked(oppRaise): # opponent raises
+        elif spriteClicked(Pbrown) and playerChips >= 100:
+            pot += 100
+            playerChips -= 100
+        elif spriteClicked(Ored) and opponentChips >= 5:
+            pot += 5
+            opponentChips -= 5
+        elif spriteClicked(Opink) and opponentChips >= 10:
+            pot += 10
+            opponentChips -= 10
+        elif spriteClicked(Oblue) and opponentChips >= 20:
+            pot += 20
+            opponentChips -= 20
+        elif spriteClicked(Opurple) and opponentChips >= 25:
+            pot += 25
+            opponentChips -= 25
+        elif spriteClicked(Oorange) and opponentChips >= 50:
             pot += 50
             opponentChips -= 50
-            changeLabel(potLabel,str(pot))
-            changeLabel(opponentLabel,str(opponentChips))
+        elif spriteClicked(Obrown) and opponentChips >= 100:
+            pot += 100
+            opponentChips -= 100
+        # done betting button, turn over
         elif spriteClicked(plaFold): # player folds
             winner = showAll(1)
         elif spriteClicked(oppFold): # opponent folds
             winner = showAll(2)
+        changeLabel(potLabel,str(pot))
+        changeLabel(playerLabel,str(playerChips))
+        changeLabel(opponentLabel,str(opponentChips))
         pause(100)
     return winner
 
@@ -295,17 +394,25 @@ def reset():
 if __name__ == '__main__':
     screenSize(1024,576)
     setBackgroundImage('felt.jpg')
-    playerChips = 10000
-    opponentChips = 10000
+    playerChips = 1000
+    opponentChips = 1000
     pot = 0
     r = 1 # round
     game = True
-    roundLabel,potLabel,playerLabel,opponentLabel,playerB,opponentB,plaFold,plaCall,plaRaise,oppFold,oppCall,oppRaise = setup()
-    flop,player,opponent,f1,f2,f3,f4,f5,p1,p2,o1,o2 = deal()
-    winner = play()
-    pause(1000)
-    hideLabel(winner)
-    # play again
+    Pchips,Ochips,potLabel,playerLabel,opponentLabel,playerB,opponentB,plaFold,plaCall,plaRaise,oppFold,oppCall,oppRaise = setup()
+    [Pred,Ppink,Pblue,Ppurple,Porange,Pbrown] = Pchips
+    [Ored,Opink,Oblue,Opurple,Oorange,Obrown] = Ochips
+    while True:
+
+        flop,player,opponent,f1,f2,f3,f4,f5,p1,p2,o1,o2 = deal()
+        winner = play()
+        pause(1000)
+        hideLabel(winner)
+        # play again
+
+# add instructions such as, must bet at least x amount to call or raise etc
+# add button to finish turm, replace call and raise buttons
+# get message if not bet enough or anything, or can't bet
 
 
 endWait()
